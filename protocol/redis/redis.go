e945504a1060e9dcf1b435ab39b2dca70da00ea3
honggang.liu <honggang.liu@shopee.com>
2024-01-06 09:15:12 +0800

    增加redis

---
+++ protocol/redis/redis.go
@@ -0,0 +1,128 @@
+package redis
+
+import (
+	"context"
+	"crypto/tls"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/redis/go-redis/v9"
+
+	"github.com/liuhailove/tc-base-go/protocol/logger"
+)
+
+var ErrNotConfigured = errors.New("Redis is not configured")
+
+// RedisConfig redis配置
+type RedisConfig struct {
+	Address           string   `yaml:"address"`
+	Username          string   `yaml:"username"`
+	Password          string   `yaml:"password"`
+	DB                int      `yaml:"db"`
+	UseTLS            bool     `yaml:"use_tls"`
+	MasterName        string   `yaml:"sentinel_master_name"`
+	SentinelUsername  string   `yaml:"sentinel_username"`
+	SentinelPassword  string   `yaml:"sentinel_password"`
+	SentinelAddresses []string `yaml:"sentinel_addresses"`
+	ClusterAddresses  []string `yaml:"cluster_addresses"`
+	DialTimeout       int      `yaml:"dial_timeout"`
+	ReadTimeout       int      `yaml:"read_timeout"`
+	WriteTimeout      int      `yaml:"write_timeout"`
+	// for clustererd mode only, number of redirects to follow, defaults to 2
+	MaxRedirects *int `yaml:"max_redirects"`
+}
+
+func (r *RedisConfig) IsConfigured() bool {
+	if r.Address != "" {
+		return true
+	}
+	if len(r.SentinelAddresses) > 0 {
+		return true
+	}
+	if len(r.ClusterAddresses) > 0 {
+		return true
+	}
+	return false
+}
+
+func (r *RedisConfig) GetMaxRedirects() int {
+	if r.MaxRedirects != nil {
+		return *r.MaxRedirects
+	}
+	return 2
+}
+
+func GetRedisClient(conf *RedisConfig) (redis.UniversalClient, error) {
+	if conf == nil {
+		return nil, nil
+	}
+
+	if !conf.IsConfigured() {
+		return nil, ErrNotConfigured
+	}
+
+	var rcOptions *redis.UniversalOptions
+	var rc redis.UniversalClient
+	var tlsConfig *tls.Config
+
+	if conf.UseTLS {
+		tlsConfig = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}
+	}
+
+	if len(conf.SentinelAddresses) > 0 {
+		logger.Infow("connecting to redis", "sentinel", true, "addr", conf.SentinelAddresses, "masterName", conf.MasterName)
+
+		// By default DialTimeout set to 2s
+		if conf.DialTimeout == 0 {
+			conf.DialTimeout = 200
+		}
+
+		// By default WriteTimeout set to 0.2s
+		if conf.WriteTimeout == 0 {
+			conf.WriteTimeout = 200
+		}
+
+		rcOptions = &redis.UniversalOptions{
+			Addrs:            conf.SentinelAddresses,
+			SentinelUsername: conf.SentinelUsername,
+			SentinelPassword: conf.SentinelPassword,
+			MasterName:       conf.MasterName,
+			Username:         conf.Username,
+			Password:         conf.Password,
+			DB:               conf.DB,
+			TLSConfig:        tlsConfig,
+			DialTimeout:      time.Duration(conf.DialTimeout) * time.Millisecond,
+			ReadTimeout:      time.Duration(conf.ReadTimeout) * time.Millisecond,
+			WriteTimeout:     time.Duration(conf.WriteTimeout) * time.Millisecond,
+		}
+	} else if len(conf.ClusterAddresses) > 0 {
+		logger.Infow("connecting to redis", "cluster", true, "addr", conf.ClusterAddresses)
+		rcOptions = &redis.UniversalOptions{
+			Addrs:        conf.ClusterAddresses,
+			Username:     conf.Username,
+			Password:     conf.Password,
+			DB:           conf.DB,
+			TLSConfig:    tlsConfig,
+			MaxRedirects: conf.GetMaxRedirects(),
+		}
+	} else {
+		logger.Infow("connecting to reds", "simple", true, "addr", conf.Address)
+		rcOptions = &redis.UniversalOptions{
+			Addrs:     []string{conf.Address},
+			Username:  conf.Username,
+			Password:  conf.Password,
+			DB:        conf.DB,
+			TLSConfig: tlsConfig,
+		}
+	}
+	rc = redis.NewUniversalClient(rcOptions)
+
+	if err := rc.Ping(context.Background()).Err(); err != nil {
+		err = errors.Wrap(err, "unable to connect to redis")
+		return nil, err
+	}
+
+	return rc, nil
+}
